135e32a02d96a5e76a80dc793dfb01c08d024f7f
RacingPlayz <72081031+Racing-Playz@users.noreply.github.com>
2020-09-29 23:53:06 -0400

    Add files via upload

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 require("dotenv").config();
 const commando = require("discord.js-commando");
 const Discord = require("discord.js");
 const querystring = require("query-string");
 const r2 = require("r2");
-const CAT_API_URL = "https://api.thecatapi.com/";
-const CAT_API_KEY = process.env.CAT_API_KEY;
+const DOG_API_URL = "https://api.thedogapi.com/";
+const DOG_API_KEY = process.env.DOG_API_KEY;
 
-module.exports = class CatCommand extends commando.Command {
+module.exports = class DogCommand extends commando.Command {
   constructor(client) {
     super(client, {
-      name: "cat",
+      name: "dog",
       group: "fun",
-      memberName: "cat",
-      description: "Meow."
+      memberName: "dog",
+      description: "Woof."
     });
   }
 
@@ -21,9 +22,11 @@ module.exports = class CatCommand extends commando.Command {
     if (message.author.bot) return;
     try {
       var images = await this.loadImage(message.author.username);
+
       var image = images[0];
+
       let embed = new Discord.MessageEmbed()
-        .setTitle("A picture of a cute kitty cat!")
+        .setTitle("A picture of a cute puppy!")
         .setImage(`${image.url}`)
         .setColor("GREEN")
       message.channel.send(embed);
@@ -34,12 +37,12 @@ module.exports = class CatCommand extends commando.Command {
 
   async loadImage(sub_id) {
     var headers = {
-      "X-API-KEY": CAT_API_KEY
+      "X-API-KEY": DOG_API_KEY
     };
     var query_params = {
-      //'has_breeds':true,
+      has_breeds: true,
       mime_types: "jpg,png",
-      size: "med",
+      size: "small",
       sub_id: sub_id,
       limit: 1
     };
@@ -47,7 +50,7 @@ module.exports = class CatCommand extends commando.Command {
     let queryString = querystring.stringify(query_params);
 
     try {
-      let _url = CAT_API_URL + `v1/images/search?${queryString}`;
+      let _url = DOG_API_URL + `v1/images/search?${queryString}`;
       var response = await r2.get(_url, { headers }).json;
     } catch (e) {
       console.log(e);
